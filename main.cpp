adb8b095e5bda24b518c60157b63d36d4ed701f7
HHR <467178874@qq.com>
2019-11-27 22:39:33 +0800

    换题目，改写AVL树

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-#include <iostream>
-#include "BigInteger.h"
+//
+// Created by HHR on 2019/11/27.
+//
 
-using namespace std;
-
-int main() {
-    string num1, num2;
-    //cin >> num1 >> num2;
-    BigInteger n1(
-            "0");
-    BigInteger n2(
-            "-12234");
-    BigInteger n3 = n1 + n2;
-    n3.display();
-    return 0;
-}
