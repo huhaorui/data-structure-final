c1c849a69ab635740c1cfc0a26c8560086f39d3a
HHR <467178874@qq.com>
2019-12-11 19:33:26 +0800

    小修小补

--- main.cpp
+++ main.cpp
@@ -24,22 +24,22 @@ void SaveFile(AvlTree *root) {
 
 void PasswordReset(Node *user) {
     string password;
-    cout << "请输入原密码";
+    cout << "请输入原密码:";
     cin >> password;
     if (password != user->getPassword()) {
         cout << "密码错误" << endl;
         return;
     }
-    cout << "请输入新密码";
+    cout << "请输入新密码:";
     cin >> password;
     user->setPassword(password);
-
-    cout << "成功";
+    cout << "成功" << endl;
+    system("pause");
 }
 
 void AddUser(AvlTree *tree) {
     string name, password;
-    cout << "请输入姓名和密码" << endl;
+    cout << "请输入姓名和密码:" << endl;
     cin >> name >> password;
     tree->addUser(name, password);
     cout << "创建成功" << endl;
@@ -50,12 +50,17 @@ void DeleteUser(AvlTree *tree) {
     string name;
     cout << "请输入姓名" << endl;
     cin >> name;
+    if (name == "admin") {
+        cout << "该用户不能被删除" << endl;
+        return;
+    }
     tree->removeUser(name);
     tree->print();
 }
 
 void adminView(AvlTree *tree) {
     while (true) {
+        system("cls");
         cout << "你好,admin" << endl;
         cout << "1.正向打印树" << endl;
         cout << "2.添加节点" << endl;
@@ -65,35 +70,46 @@ void adminView(AvlTree *tree) {
         cin >> op;
         switch (op) {
             case '1':
+                system("cls");
                 tree->print();
+                system("pause");
                 break;
             case '2':
+                system("cls");
                 AddUser(tree);
+                system("pause");
                 break;
             case '3':
+                system("cls");
                 DeleteUser(tree);
+                system("pause");
                 break;
             case '4':
-                exit(0);
+                break;
             default:
                 cout << "错误" << endl;
         }
+        if (op == '4')break;
         SaveFile(tree);
     }
 }
 
 void userView(Node *user, AvlTree *tree) {
-    cout << "你好," << user->getName() << endl;
-    cout << "1.修改密码" << endl;
-    cout << "2.退出" << endl;
-    char op;
-    cin >> op;
-    if (op == '1') {
-        PasswordReset(user);
-        SaveFile(tree);
-    } else {
-        cout << "再见" << endl;
-        exit(0);
+    while (true) {
+        system("cls");
+        cout << "你好," << user->getName() << endl;
+        cout << "1.修改密码" << endl;
+        cout << "2.退出" << endl;
+        char op;
+        cin >> op;
+        if (op == '1') {
+            system("cls");
+            PasswordReset(user);
+            SaveFile(tree);
+        } else {
+            cout << "再见" << endl;
+            break;
+        }
     }
 }
 
@@ -112,6 +128,7 @@ void login(AvlTree *tree) {
     Node *user = AvlTree::SearchNode(name, tree->root);
     if (user == nullptr) {
         cout << "用户不存在" << endl;
+        system("pause");
         return;
     }
     cout << "请输入密码:";
@@ -121,32 +138,30 @@ void login(AvlTree *tree) {
         loginSuccessful(user, tree);
     } else {
         cout << "密码错误" << endl;
+        system("pause");
     }
 }
 
-bool menu(AvlTree *tree) {
-    char op;
-    cout << "1.登陆" << endl;
-    cout << "2.退出" << endl;
-    cin >> op;
-    system("cls");
-    switch (op) {
-        case '1':
-            login(tree);
-        case '2':
-            return false;
-        default:
-            cout << "错误" << endl;
-            system("pause");
-    }
-    return true;
-}
 
 int main() {
     auto *tree = new AvlTree;
     readFile(tree);
-    tree->print();
-    system("pause");
-    while (menu(tree)) {
+    while (true) {
+        char op;
+        system("cls");
+        cout << "1.登陆" << endl;
+        cout << "2.退出" << endl;
+        cin >> op;
+        system("cls");
+        switch (op) {
+            case '1':
+                login(tree);
+                break;
+            case '2':
+                exit(0);
+            default:
+                cout << "错误" << endl;
+                system("pause");
+        }
     }
 }
